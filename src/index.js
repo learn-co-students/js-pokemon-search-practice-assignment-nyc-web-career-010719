b939bc3097ed8b5833cc51d15b6e41cf657adb5f
cannoli9917 <katie.pennachio@gmail.com>
2019-02-20 17:12:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log(POKEMON)
   //YOUR CODE HERE
+
+
+  const pokemonContainer = document.querySelector("#pokemon-container")
+
+  const form = document.querySelector("#pokemon-search-form")
+
+  const input = document.querySelector("#pokemon-search-input")
+
+  displayPokemon(POKEMON)
+
+  input.addEventListener("input", function(event) {
+    // event.preventDefault()
+    while (pokemonContainer.firstChild) {
+    pokemonContainer.removeChild(pokemonContainer.firstChild)
+    }
+    let filter = input.value.toLowerCase()
+    let filteredPokemon = POKEMON.filter(function(pokemon) {
+      return pokemon.name.indexOf(filter) > -1
+    })
+    if (filteredPokemon.length > 0) {
+      displayPokemon(filteredPokemon)
+    }
+    else if (filteredPokemon.length = 0) {
+      document.querySelector('p')
+    }
+  })
+// if filter is something show filtered cards
+
+
+function displayPokemon(group) {
+
+
+  group.forEach(function (pokemon) {
+
+    let pokemonCard = document.createElement("div")
+    pokemonCard.setAttribute("class", "pokemon-card")
+    pokemonContainer.appendChild(pokemonCard)
+
+    let pokemonFrame = document.createElement("div")
+    pokemonFrame.setAttribute("class", "pokemon-frame")
+    pokemonCard.appendChild(pokemonFrame)
+
+    let pokemonName = document.createElement("h1")
+    pokemonName.setAttribute("class", "center-text")
+    pokemonName.innerText = pokemon.name
+    pokemonFrame.appendChild(pokemonName)
+
+    let pokemonImage = document.createElement("div")
+    pokemonImage.setAttribute("class", "pokemon-image")
+    pokemonFrame.appendChild(pokemonImage)
+
+    let image = document.createElement("img")
+    image.setAttribute("class", "toggle-sprite")
+    image.setAttribute("data-action", "flip")
+    image.setAttribute("data-id", pokemon.id)
+    image.setAttribute("src", pokemon.sprites.front)
+    pokemonImage.appendChild(image)
+
+    image.addEventListener("click", sprite)
+
+    function sprite() {
+      if (image.src === pokemon.sprites.front) {
+        image.src = pokemon.sprites.back
+      }
+      else if (image.src === pokemon.sprites.back) {
+        image.src = pokemon.sprites.front
+      }
+    }
+
+  })}
+
+
 })
